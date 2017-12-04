2a5a1dccfdd10963d1533e1053b4bf08d899804e
Steven Warmelink <stevenwarmelink@gmail.com>
2017-12-04 10:24:20 +0100

    lab3

---
+++ lab3/assignment1.m
@@ -0,0 +1,71 @@
+%% Question 1-2: Let the value of x* be equal to 10 and 
+%  compute the probabilities of hit (h) and false alarm (fa)
+
+format long;
+
+mu_1 = 5;
+mu_2 = 7;
+
+var_1 = 4;
+var_2 = 4;
+
+% calculate hit
+false_alarm  = 1 - normcdf(10,mu_1, sqrt(var_1))
+
+hit          = 1 - normcdf(10,mu_2, sqrt(var_2))
+
+%Hit =  probability gets classified as W1
+%FA  =  probability gets classified as W2
+
+
+%% Question 3-5: Discriminibility
+
+% Question 3
+mu_1 = 5;
+mu_2 = 7;
+var  = 4;
+
+discr = abs((mu_1 - mu_2) / sqrt(var))
+
+% Question 4
+mu_1 = 5;
+mu_2 = 9;
+var  = 4;
+
+discr = abs((mu_1 - mu_2) / sqrt(var))
+
+% Question 5
+
+mu_1 = 5;
+mu_2 = 11;
+var  = 4;
+
+discr = abs((mu_1 - mu_2) / sqrt(var))
+
+%% Question 6-10
+
+% Question 6
+% -Knowing the coordinates of a point in the ROC space is sufficient for calculating the classification accuracy at that point.
+% -A random-guess classifier would have an ROC that falls on the diagonal (black line).
+
+% Question 7
+data = load('lab3_1.mat');
+data = data.outcomes;
+
+% 1 1 = hit
+% 1 0 = false negative
+% 0 1 = false positive
+% 0 0 = no signal
+
+n_hits = 0;
+
+for idx = 1:length(data)
+    if (data(idx,1) == 1) && (data(idx,2) == 1)
+        n_hits = n_hits + 1;
+    end
+end
+
+hitrate = n_hits / length(data)
+
+% Question 8
+
