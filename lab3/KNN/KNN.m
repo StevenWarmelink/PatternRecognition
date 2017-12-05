a7eb6fbd4d1ad7c4961768c85ced466a4e6bd96a
Steven Warmelink <stevenwarmelink@gmail.com>
2017-12-05 17:22:15 +0100

    KNN update (LOOCV in progress)

--- lab3/KNN/KNN.m
+++ lab3/KNN/KNN.m
@@ -1,10 +1,5 @@
 function output = KNN ( Coordinates, K, data, class_labels)
-    dist = zeros(1,length(data));
-    for idx = 1:length(data)
-        dist(idx) = norm(data(idx,:) - Coordinates);
-    end 
-    [B, I] = sort(dist,'ascend');
-    total = class_labels(I(1:K));
-
-    output = mode(total);
+    dist = pdist2(Coordinates, data); 
+    [~, I] = sort(dist,'ascend');
+    output = mode(class_labels(I(1:K)));
 end
--- lab3/KNN/knn_wrapper.m
+++ lab3/KNN/knn_wrapper.m
@@ -1,7 +1,7 @@
 clear all;
 load lab3_2.mat;
 
-K=5;
+K=3;
 samples=64;
 data = lab3_2;
 nr_of_classes = 2;
@@ -28,3 +28,47 @@ title([int2str(K) '-NN, ' int2str(nr_of_classes) ' classes']);
 scaled_data=samples*data;
 plot(scaled_data(  1:100,1),scaled_data(  1:100,2),'go');
 plot(scaled_data(101:200,1),scaled_data(101:200,2),'r+');
+
+%% LOOCV for k = 3;
+
+clear all;
+load lab3_2.mat;
+
+K=3;
+samples=64;
+data = lab3_2;
+nr_of_classes = 2;
+
+% Class labels
+class_labels = floor( (0:length(data)-1) * nr_of_classes / length(data) );
+
+% Sample the parameter space
+result    = zeros(samples);
+subresult = zeros(samples);
+min_error = 10;
+min_idx = 0;
+for datapoint = 1:length(data)
+    numerrors = 0;
+    for i=1:samples
+    X=(i-1/2)/samples;
+        for j=1:samples
+            Y=(j-1/2)/samples;
+            loc_dat = reshape(data, [200, 2]);
+            loc_lab = class_labels;
+            loc_dat(datapoint,:) = [];
+            loc_lab(datapoint) = [];
+            subresult(j,i) = KNN(data(datapoint,:),K,loc_dat,loc_lab);
+            if class_labels(datapoint) ~= subresult(j,i)
+                numerrors = numerrors + 1;
+            end
+        end
+    end;
+    error_rate = numerrors / length(data);
+    if error_rate < min_error
+        min_error = error_rate;
+        min_idx   = datapoint;
+    end
+    disp(datapoint);
+end;
+disp(min_error);
+disp(min_idx);
