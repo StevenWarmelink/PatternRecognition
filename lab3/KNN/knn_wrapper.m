627860dd0f319c77f09d27628810768c18dd8970
Steven Warmelink <stevenwarmelink@gmail.com>
2017-12-08 22:30:40 +0100

    KNN update

--- lab3/KNN/knn_wrapper.m
+++ lab3/KNN/knn_wrapper.m
@@ -1,13 +1,15 @@
+%% Options
 clear all;
 load lab3_2.mat;
 
-K=3;
+K=1;
 samples=64;
 data = lab3_2;
-nr_of_classes = 2;
-
-% Class labels
+nr_of_classes = 4;
 class_labels = floor( (0:length(data)-1) * nr_of_classes / length(data) );
+
+
+%% Generate plot for given K
 
 % Sample the parameter space
 result=zeros(samples);
@@ -20,100 +22,26 @@ for i=1:samples
 end;
 
 % Show the results in a figure
+figure(1);
 imshow(result,[0 nr_of_classes-1],'InitialMagnification','fit')
 hold on;
 title([int2str(K) '-NN, ' int2str(nr_of_classes) ' classes']);
 
 % this is only correct for the first question
 scaled_data=samples*data;
-plot(scaled_data(  1:100,1),scaled_data(  1:100,2),'go');
-plot(scaled_data(101:200,1),scaled_data(101:200,2),'r+');
+if nr_of_classes == 2
+    plot(scaled_data(  1:100,1),scaled_data(  1:100,2),'go');
+    plot(scaled_data(101:200,1),scaled_data(101:200,2),'r+');
+else
+    plot(scaled_data(  1:50,1),scaled_data(  1:50,2),'go');
+    plot(scaled_data( 51:100,1),scaled_data(  51:100,2),'cs');
+    plot(scaled_data(101:150,1),scaled_data(101:150,2),'y+');
+    plot(scaled_data(151:200,1),scaled_data(151:200,2),'bd');
+end
+hold off;
 
-%% LOOCV for k = 3;
 
-clear all;
-load lab3_2.mat;
-
-K=3;
-samples=64;
-data = lab3_2;
-nr_of_classes = 2;
-
-% Class labels
-class_labels = floor( (0:length(data)-1) * nr_of_classes / length(data) );
-
-% Sample the parameter space
-K_vals = 1:1:25;
-e_rates = zeros(length(K_vals),1);
-for k_val = 1:length(K_vals)
-    numerrors = 0;
-    for datapoint = 1:length(data)
-        loc_dat = reshape(data, [200, 2]);
-        loc_dat(datapoint,:) = [];
-        loc_lab = class_labels;
-        loc_lab(datapoint) = [];
-        result = KNN(data(datapoint,:),K_vals(k_val),loc_dat,loc_lab);
-        if class_labels(datapoint) ~= result
-            numerrors = numerrors + 1;
-        end
-    end
-    error_rate = numerrors / length(data);
-    e_rates(k_val) = error_rate;
-    K_vals(k_val)
-end;
-[B I] = min(e_rates);
-figure();
-plot(K_vals,e_rates);
-xlabel('K values');
-ylabel('Error rate');
-title({'KNN classification rate using LOOCV','for different K with 2 classes'});
-%% 4 classes!!!
-
-clear all;
-load lab3_2.mat;
-
-K=7;
-samples=64;
-data = lab3_2;
-nr_of_classes = 4;
-
-% Class labels
-class_labels = floor( (0:length(data)-1) * nr_of_classes / length(data) );
-
-% Sample the parameter space
-result=zeros(samples);
-for i=1:samples
-  X=(i-1/2)/samples;
-  for j=1:samples
-    Y=(j-1/2)/samples;
-    result(j,i) = KNN([X Y],K,data,class_labels);
-  end;
-end;
-
-% Show the results in a figure
-imshow(result,[0 nr_of_classes-1],'InitialMagnification','fit')
-hold on;
-title([int2str(K) '-NN, ' int2str(nr_of_classes) ' classes']);
-
-% this is only correct for the first question
-scaled_data=samples*data;
-plot(scaled_data(  1:50,1),scaled_data(  1:50,2),'go');
-plot(scaled_data( 51:100,1),scaled_data(  51:100,2),'cs');
-plot(scaled_data(101:150,1),scaled_data(101:150,2),'y+');
-plot(scaled_data(151:200,1),scaled_data(151:200,2),'bd');
-
-%% LOOCV for 4 classes 
-
-clear all;
-load lab3_2.mat;
-
-K=3;
-samples=64;
-data = lab3_2;
-nr_of_classes = 4;
-
-% Class labels
-class_labels = floor( (0:length(data)-1) * nr_of_classes / length(data) );
+%% LOOCV
 
 % Sample the parameter space
 K_vals = 1:1:25;
@@ -132,11 +60,10 @@ for k_val = 1:length(K_vals)
     end
     error_rate = numerrors / length(data);
     e_rates(k_val) = error_rate;
-    K_vals(k_val)
 end;
 [B I] = min(e_rates);
-K_vals(I)
+figure(2);
 plot(K_vals,e_rates);
-xlabel('K values');
+xlabel('Number of neighbours (K) in KNN');
 ylabel('Error rate');
-title({'KNN classification rate using LOOCV','for different K with 4 classes'});
+title(['KNN Error rate using LOOCV for different K with ',num2str(nr_of_classes),' class(es)']);
