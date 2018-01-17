fe39a20260dd53f8e93700cd2dfbe176df82c2ce
Steven Warmelink <stevenwarmelink@gmail.com>
2018-01-17 17:40:27 +0100

    fix assignment 3

--- lab7/assignment3.m
+++ lab7/assignment3.m
@@ -7,50 +7,67 @@ close all;
 addpath(genpath('.'));
 
 % set vectors
-x1 = [0 0]';
-x2 = [2 3]';
-x3 = [1 4]';
-x4 = [4 2]';
-x5 = [3 0]';
+x1 = [0 0];
+x2 = [2 3];
+x3 = [1 4];
+x4 = [4 2];
+x5 = [3 0];
 
+
+%% Explorative: plot data
 % create data vector
-data = [x1 x2 x3 x4 x5];
+data = [x1; x2; x3; x4; x5];
+
+
+% plot data
+figure(1);
+scatter(data(:,1),data(:,2));
+xlim([-1 5]);
+ylim([-1 5]);
 %% question 1
 
 % create cluster division
-avg_c1_q1 = (x1 + x2 + x3) / 3;
-avg_c2_q1 = (x4 + x5)      / 2;
+c1 = [x1;x2;x3];
+c2 = [x4;x5];
+avg_c1 = (x1 + x2 + x3) / 3;
+avg_c2 = (x4 + x5)      / 2;
 
 % print result
 disp('Question 1');
-calculateJe(data, avg_c1_q1,avg_c2_q1);
+calculateJe(c1, c2, avg_c1,avg_c2);
 
 %% question 2
 
 % create cluster division
-avg_c1_q2 = (x2 + x3 + x5) / 3;
-avg_c2_q2 = (x1 + x4)      / 2;
+c1 = [x2; x3; x5];
+c2 = [x1; x4];
+avg_c1 = (x2 + x3 + x5) / 3;
+avg_c2 = (x1 + x4)      / 2;
 
 % print result
 disp('Question 2');
-calculateJe(data, avg_c1_q2,avg_c2_q2);
+calculateJe(c1, c2, avg_c1,avg_c2);
 
 %% question 3
 
 % create cluster division
-avg_c1_q3 = x4;
-avg_c2_q3 = (x1 + x2 + x3 + x5) / 4;
+c1 = x4;
+c2 = [x1; x2; x3; x5];
+avg_c1 = x4;
+avg_c2 = (x1 + x2 + x3 + x5) / 4;
 
 % print result
 disp('Question 3');
-calculateJe(data, avg_c1_q3,avg_c2_q3);
+calculateJe(c1, c2, avg_c1,avg_c2);
 
 %% question 4
 
 % create cluster division
-avg_c1_q4 = (x3 + x5)       / 2;
-avg_c2_q4 = (x1 + x2 + x4)  / 3;
+c1 = [x3; x5];
+c2 = [x1; x2; x4];
+avg_c1 = (x3 + x5)       / 2;
+avg_c2 = (x1 + x2 + x4)  / 3;
 
 % print result
 disp('Question 4');
-calculateJe(data, avg_c1_q4,avg_c2_q4);
+calculateJe(c1, c2, avg_c1,avg_c2);
--- lab7/src/calculateJe.m
+++ lab7/src/calculateJe.m
@@ -1,12 +1,22 @@
-function calculateJe(data, c1, c2)
+function calculateJe(c1, c2, m1, m2)
+
+% Initialize variables for saving error per cluster
+sum_c1 = 0;
+sum_c2 = 0;
+
+[rows1, ~] = size(c1);
+[rows2, ~] = size(c2);
 
 % Calculate squared deviation from mean
-sum_c1 = abs(data - c1).^2;
-sum_c2 = abs(data - c2).^2;
+for point = 1:rows1
+    sum_c1 = sum_c1 + pdist2(c1(point,:),m1,'squaredeuclidean');
+end
+for point = 1:rows2
+    sum_c2 = sum_c2 + pdist2(c2(point,:),m2,'squaredeuclidean');
+end
 
 % Sum squared deviations
 total = sum_c1 + sum_c2;
-total = sum(total,2);
 
 % Print output
 disp(total);
